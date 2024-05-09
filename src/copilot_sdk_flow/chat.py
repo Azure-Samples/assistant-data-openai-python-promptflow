3fdf86a8cdd6db51d92181aa55225f1ddf81c70d
Jeff Omhover <jf.omhover@gmail.com>
2024-05-09 11:30:41 -0700

    upgrade requriements, add context as input

--- src/copilot_sdk_flow/chat.py
+++ src/copilot_sdk_flow/chat.py
@@ -157,7 +157,7 @@ class AssistantThreadRunner(object):
                     },
                     "context": {
                         "thread_id": self.thread.id,
-                        "steps": self.messages_during_loop,
+                        # "steps": self.messages_during_loop,
                     },
                 }
             ],
@@ -264,17 +264,26 @@ def chat_completion(
     if "thread_id" in context:
         logging.info(f"Using thread_id from context: {context['thread_id']}")
         thread = trace(aoai_client.beta.threads.retrieve)(context["thread_id"])
+
+        # Add last message in the thread
+        logging.info("Adding last message in the thread")
+        _ = trace(aoai_client.beta.threads.messages.create)(
+            thread_id=thread.id,
+            role=messages[-1]["role"],
+            content=messages[-1]["content"],
+        )
     else:
         logging.info(f"Creating a new thread")
         thread = trace(aoai_client.beta.threads.create)()
-
-    # Add last message in the thread
-    logging.info("Adding last message in the thread")
-    _ = trace(aoai_client.beta.threads.messages.create)(
-        thread_id=thread.id,
-        role=messages[-1]["role"],
-        content=messages[-1]["content"],
-    )
+        
+        # Add all messages in the thread
+        logging.info("Adding all messages in the thread")
+        for message in messages:
+            _ = trace(aoai_client.beta.threads.messages.create)(
+                thread_id=thread.id,
+                role=message["role"],
+                content=message["content"],
+            )
 
     # Create a run in the thread
     logging.info("Creating the run")
