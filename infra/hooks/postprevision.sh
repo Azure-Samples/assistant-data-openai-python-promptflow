3c91890319f8b6d9a814cd7278f58d00aadd8c65
Jeff Omhover <jeomhove@microsoft.com>
2024-05-15 08:21:19 -0700

    add pip install in postprevision

--- infra/hooks/postprevision.sh
+++ infra/hooks/postprevision.sh
@@ -1,2 +1,7 @@
 echo "Copy .env file from ./.azure/$AZURE_ENV_NAME/.env to root"
 cp ./.azure/$AZURE_ENV_NAME/.env ./.env
+
+echo "Script execution completed successfully."
+
+echo "Installing dependencies from 'src/requirements.txt'"
+python -m pip install -r ./src/requirements.txt
--- infra/hooks/postprovision.ps1
+++ infra/hooks/postprovision.ps1
@@ -1,2 +1,6 @@
 Write-Host "Copy .env file from ./.azure/$Env:AZURE_ENV_NAME/.env to root"
 Copy-Item -Path ./.azure/$Env:AZURE_ENV_NAME/.env -Destination ./.env -Force
+
+
+Write-Host "Installing dependencies from 'src/requirements.txt'"
+python -m pip install -r ./src/requirements.txt
