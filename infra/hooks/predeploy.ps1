129964220b6f79a3d062ccc40d5aeb9d2cf0d53a
Jeff Omhover <jeomhove@microsoft.com>
2024-05-15 08:42:11 -0700

    better logs

--- infra/hooks/predeploy.ps1
+++ infra/hooks/predeploy.ps1
@@ -1,8 +1,8 @@
 Write-Host "Copy .env file from ./.azure/$Env:AZURE_ENV_NAME/.env to root"
 Copy-Item -Path ./.azure/$Env:AZURE_ENV_NAME/.env -Destination ./.env -Force
 
-Write-Host "Create an assistant and add to env ./.env"
+Write-Host "Create an assistant and add its identifier as env var in ./.env"
 python ./src/create_assistant.py --export-env ./.env
 
-Write-Host "Running python script"
+Write-Host "Running python script to deploy the code into the endpoint"
 python ./src/deploy.py
--- infra/hooks/predeploy.sh
+++ infra/hooks/predeploy.sh
@@ -1,8 +1,8 @@
 echo "Copy .env file from ./.azure/$AZURE_ENV_NAME/.env to root"
 cp ./.azure/$AZURE_ENV_NAME/.env ./.env
 
-echo "Create an assistant and add to env ./.env"
+echo "Create an assistant and add its identifier as env var in ./.env"
 python ./src/create_assistant.py --export-env ./.env
 
-echo "Running python script"
+echo "Running python script to deploy the code into the endpoint"
 python ./src/deploy.py
