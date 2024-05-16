e99ff7cd57e36174bfed9b07b86472b39eef4627
Jeff Omhover <jf.omhover@gmail.com>
2024-05-15 20:28:50 -0700

    move assistant creation in azd provision

--- infra/hooks/postprevision.sh
+++ infra/hooks/postprevision.sh
@@ -1,7 +1,10 @@
 echo "Copy .env file from ./.azure/$AZURE_ENV_NAME/.env to root"
 cp ./.azure/$AZURE_ENV_NAME/.env ./.env
 
-echo "Script execution completed successfully."
-
 echo "Installing dependencies from 'src/requirements.txt'"
 python -m pip install -r ./src/requirements.txt
+
+Write-Host "Create an assistant and add its identifier as env var in ./.env"
+python ./src/create_assistant.py --export-env ./.env
+
+echo "Script execution completed successfully."
--- infra/hooks/postprovision.ps1
+++ infra/hooks/postprovision.ps1
@@ -1,6 +1,10 @@
 Write-Host "Copy .env file from ./.azure/$Env:AZURE_ENV_NAME/.env to root"
 Copy-Item -Path ./.azure/$Env:AZURE_ENV_NAME/.env -Destination ./.env -Force
 
-
 Write-Host "Installing dependencies from 'src/requirements.txt'"
 python -m pip install -r ./src/requirements.txt
+
+Write-Host "Create an assistant and add its identifier as env var in ./.env"
+python ./src/create_assistant.py --export-env ./.env
+
+Write-Host "Script execution completed successfully."
